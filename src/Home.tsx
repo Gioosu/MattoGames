38947a1e257767725f582a9a3b727ddc45cce794
Gioosu <128608715+Gioosu@users.noreply.github.com>
2026-07-14 22:31:13 +0200

    minor fix

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "./assets/MG_nobg_white.webp";
+import logo from "./assets/mg_nobg_white.webp";
 
 const taglines: string[] = [
   "Siete pronti a litigare brutalmente?",
